8f8c333555e025d285b392d7cc187be930b26175
JayPankajPatel <JayP102501@gmail.com>
2022-04-09 18:04:09 -0700

    Added TCP python object sharing

--- TCP/client.py
+++ TCP/client.py
@@ -1,7 +1,36 @@
 import socket
+import time
+import pickle
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #IPv4 and TCP
-client.connect(("192.168.1.86", 9999))
+def client(IP :str, port_num: int, decode: str) -> None:
+    HEADERSIZE = 10
 
-msg = client.recv(1024)
-print(msg.decode("utf-8"))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, port_num))
+
+    while True:
+        full_msg = b''
+        new_msg = True
+        while True:
+            msg = s.recv(16)
+            if new_msg:
+                print("new msg len:",msg[:HEADERSIZE])
+                msglen = int(msg[:HEADERSIZE])
+                new_msg = False
+
+            print(f"full message length: {msglen}")
+
+            full_msg += msg
+
+            print(len(full_msg))
+
+            if len(full_msg)-HEADERSIZE == msglen:
+                print("full msg recvd")
+                print(full_msg[HEADERSIZE:])
+                print(pickle.loads(full_msg[HEADERSIZE:]))
+                new_msg = True
+                full_msg = b""
+
+
+if __name__ == "__main__":
+    client("192.168.1.86",9999,"utf-8")
--- TCP/server.py
+++ TCP/server.py
@@ -1,13 +1,28 @@
 import socket
+import time
+import pickle
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #IPv4 and TCP
-server.bind(("192.168.1.86", 9999))
 
-server.listen(5)
 
-while True:
-    clientsocket, address = server.accept()
-    print(f"Connection from {address} has been made")
-    clientsocket.send("You are in my Christian Minecraft Server".encode("utf-8"))
-    clientsocket.close()
+def server(IP: str, port_num: int, encode: str, any_obj :any) -> None:
+    HEADERSIZE = 10
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((IP, port_num))
+    s.listen(5)
+
+    while True:
+        # now our endpoint knows about the OTHER endpoint.
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established.")
+
+        d = any_obj
+        msg = pickle.dumps(d)
+        msg = bytes(f"{len(msg):<{HEADERSIZE}}", encode)+msg
+        print(msg)
+        clientsocket.send(msg)
+        
+
+
+if __name__ == "__main__":
+    server("192.168.1.86",9999,"utf-8", {1:"hi", 2: "there"})
