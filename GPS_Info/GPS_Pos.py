eac9b79d1c45fea82839ba5885afe66ee52426a2
JayPankajPatel <jayp102501@gmail.com>
2022-05-01 12:00:45 -0700

    git

--- GPS_Info/GPS_Pos.py
+++ GPS_Info/GPS_Pos.py
@@ -6,9 +6,10 @@ ser = serial.Serial("/dev/ttyTHS1",9600)
 def get_cord():
     while True:
         line = ser.readline()
-        line = str(line,encoding='utf-8')
         print(line)
-        print(end="")
+        line = str(line,encoding='utf-8')
+        
+       
         if line.startswith('$GNRMC'):
             rmc = pynmea2.parse(line)
             print("Latitude:  ", rmc.latitude)
--- GPS_Module/Coordinates/GPS_COORDS.py
+++ GPS_Module/Coordinates/GPS_COORDS.py
@@ -1,16 +1,20 @@
 import serial
 import pynmea2
 import time
-ser = serial.Serial("/dev/ttyTHS1",9600)
+def get_coors() -> None:
+    ser = serial.Serial("/dev/ttyTHS1",9600)
 
-while True:
-    line = ser.readline()
-    line = str(line,encoding='utf-8')
-    print(line)
-    print(end="")
-    if line.startswith('$GNRMC'):
-        rmc = pynmea2.parse(line)
-        print("Latitude:  ", rmc.latitude)
-        print("Longitude: ", rmc.longitude)
-    time.sleep(1)
-     
+    while True:
+        line = ser.readline()
+        line = str(line,encoding='utf-8')
+        print(line)
+        print(end="")
+        if line.startswith('$GNRMC'):
+            rmc = pynmea2.parse(line)
+            print("Latitude:  ", rmc.latitude)
+            print("Longitude: ", rmc.longitude)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    get_coors()
