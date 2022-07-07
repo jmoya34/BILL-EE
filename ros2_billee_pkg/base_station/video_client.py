6d8db8d643d76672c16cc6ad4f40b4fb564c0f6e
Jason Moya <jasonmoya34@gmail.com>
2022-07-07 14:40:34 -0700

    Base and operations folder added

---
+++ ros2_billee_pkg/base_station/video_client.py
@@ -0,0 +1,31 @@
+import socket
+import cv2
+import pickle
+import struct
+import imutils
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = socket.gethostname()
+port = 10050
+client_socket.connect((host_ip,port)) 
+data = b""
+payload_size = struct.calcsize("Q")
+
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024)
+        if not packet: break
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Receiving...",frame)
+    key = cv2.waitKey(10) 
+    if key  == 13:
+        break
+client_socket.close()
